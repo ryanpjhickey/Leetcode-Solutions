189b649b1ed2665d7c6ac8431ec730d509dde531
ryanpjhickey <ryanpjhickey@gmail.com>
2022-12-28 22:37:23 -0800

    upload Java commentary to Richest Customer Wealth solution, minor code refactor after research

--- Solutions/Java Solutions/Richest Customer Wealth/solution.java
+++ Solutions/Java Solutions/Richest Customer Wealth/solution.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int wealthMax = 1;
-        int rows = accounts.length;
-        int columns = accounts[0].length;
-        for (int index=0; index < rows; index++) {
+
+        int wealthMax = 0; // define our "res" as 0 initially, use as starting point for our wealthMax
+        int rows = accounts.length; //define accounts.length as "rows"
+        int columns = accounts[0].length; //define accounts[0].length as "columns"
+
+        for (int index=0; index < rows; index++) { //iterate through rows of customers
             int wealthTemp = 0;
-            for (int z=0; z < columns; z++) {
+            for (int z=0; z < columns; z++) { //iterate through columns of customers
                 wealthTemp += accounts[index][z];
             }
-            wealthMax = Math.max(wealthMax, wealthTemp);
+            wealthMax = Math.max(wealthMax, wealthTemp); //update wealthMax whenever it is larger than the prior value stored in wealthMax
         }
-        return wealthMax;
+        return wealthMax; //return our "res," wealthMax
     }
 }
