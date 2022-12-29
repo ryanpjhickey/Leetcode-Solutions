131edbcae8d4b8cfa02b9f969ab145bcf903d4c4
ryanpjhickey <ryanpjhickey@gmail.com>
2022-12-28 23:08:44 -0800

    update solution2.java to fix broken code, add commentary explaining code

--- Solutions/Java Solutions/Richest Customer Wealth/solution2.java
+++ Solutions/Java Solutions/Richest Customer Wealth/solution2.java
@@ -1,11 +1,12 @@
 class Solution2 {
     public int maximumWealth(int[][] accounts) {
         
-        int wealthMax = Arrays.stream(accounts). //returns sequential stream of array
-        mapToInt(wealth -> Arrays.stream(wealth). //use mapToInt...
-        sum()).max().getAsInt(); //... so that we can use sum, max, and getAsInt, to return our Richest Customer Wealth as int wealthMax!
+        int wealthMax = Arrays.stream(accounts) //convert array to stream of int[]
+        .mapToInt(wealth -> Arrays.stream(wealth).sum()) //use mapToInt to reduce sum
+        .max() //find our richest customer
+        .getAsInt(); //get our richest customer as integer value
 
-        return wealthMax;
-
+        return wealthMax; //return our Richest Customer value
+        
     }
 }
