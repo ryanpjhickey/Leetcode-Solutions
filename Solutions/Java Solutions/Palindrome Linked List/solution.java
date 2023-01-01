05ab9068765250e6310218fb3990b30413cafbee
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-01 14:11:50 -0800

    upload commentary for Palindrome Linked List

--- Solutions/Java Solutions/Palindrome Linked List/solution.java
+++ Solutions/Java Solutions/Palindrome Linked List/solution.java
@@ -11,33 +11,36 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
-        ListNode slowPointer = head, fastPointer = head, previousNode, tempVar;
+        ListNode slowPointer = head; //create our pointers and other useful vars
+        ListNode fastPointer = head;
+        ListNode previousNode;
+        ListNode temp;
 
-        while (fastPointer != null && fastPointer.next != null) {
+        while (fastPointer != null && fastPointer.next != null) { //while fast pointer and fastpointer.next both have values, make both pointers move 1 & 2 respectively
             slowPointer = slowPointer.next;
             fastPointer = fastPointer.next.next;
         }
 
-        previousNode = slowPointer;
-        slowPointer = slowPointer.next;
-        previousNode.next = null;
+        previousNode = slowPointer; //set our prev node to slowPointer location
+        slowPointer = slowPointer.next; //set our slowPointer to slowPointer next. It is moving and prevNode will be the old slowPointer location.
+        previousNode.next = null; //define prevNode.next as null to lock prevNode in it's current position
 
-        while (slowPointer != null) {
-            tempVar = slowPointer.next;
-            slowPointer.next = previousNode;
-            previousNode = slowPointer;
-            slowPointer = tempVar;
+        while (slowPointer != null) { //As long as slowPointer still has a value, set temp = slowPointer.next = prevNode = slowPointer
+            temp = slowPointer.next; //set our temp to slowPointer.next
+            slowPointer.next = previousNode; //set slowPointer.next to prevNode
+            previousNode = slowPointer; //set prevNode to our slowPointer
+            slowPointer = temp; //set slowPointer to temp
         }
 
-        fastPointer = head;
-        slowPointer = previousNode;
+        fastPointer = head; //reset our fastPointer to head
+        slowPointer = previousNode; //reset our slowPointer to previousNode
 
-        while (slowPointer != null) {
-            if (fastPointer.val != slowPointer.val) return false;
-            fastPointer = fastPointer.next;
-            slowPointer = slowPointer.next;
+        while (slowPointer != null) { //if slowPointer still has a value, loop through the list
+            if (fastPointer.val != slowPointer.val) return false; //if vals are not a palindrome, return false
+            fastPointer = fastPointer.next; //move fastPointer over to check next node
+            slowPointer = slowPointer.next; //move slowPointer over to check next node
         }
         
-        return true;
+        return true; //return true if it is a palindrome
     }
 }
