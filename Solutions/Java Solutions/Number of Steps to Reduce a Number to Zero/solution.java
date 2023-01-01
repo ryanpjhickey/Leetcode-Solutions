1e953d5dbc52346743db86eebb1d766c6b185eb9
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-01 14:27:03 -0800

    upload formatting and solution commentary for Number of Steps solution

--- Solutions/Java Solutions/Number of Steps to Reduce a Number to Zero/solution.java
+++ Solutions/Java Solutions/Number of Steps to Reduce a Number to Zero/solution.java
@@ -1,14 +1,20 @@
 class Solution {
-public int numberOfSteps (int num) {
-        int steps = 0;
-        while (num != 0) {
-            if (num % 2 == 0) {
+    public int numberOfSteps (int num) {
+
+        int steps = 0; //define initial steps as 0
+
+        while (num != 0) //while num is not 0, do the following until num = 0
+        {
+            if (num % 2 == 0)  //if num divded by 2 has no remaider, divide by 2
+            {
                 num /= 2;
-            } else {
+            } 
+            else //otherwise, set num to num-1
+            {
                 num--;
             }
-            steps++;
+            steps++; //steps + 1
         }
-        return steps;
+        return steps; //return value of steps needed to reduce to 0
     }
 }
