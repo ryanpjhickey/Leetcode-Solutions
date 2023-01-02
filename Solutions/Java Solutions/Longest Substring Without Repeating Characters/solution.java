27aee3d2a477d6a81ce59c3f82dada5d56e6166d
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-01 17:40:25 -0800

    add commentary to Longest substring

--- Solutions/Java Solutions/Longest Substring Without Repeating Characters/solution.java
+++ Solutions/Java Solutions/Longest Substring Without Repeating Characters/solution.java
@@ -1,17 +1,17 @@
 class Solution {
 public int lengthOfLongestSubstring(String s) {
 
-        int index = 0;
+        int index = 0; //create our useful vars
         int pointer = 0;
         int result = 0;
         
-        Set<Character> hashSet = new HashSet<>();
+        Set<Character> hashSet = new HashSet<>(); //create our hashSet
     
-        while (pointer < s.length()) {
+        while (pointer < s.length()) { //while pointer is less than s.length, check if hashSet contains a character at the pointer value, and then update our max substring
             if (!hashSet.contains(s.charAt(pointer))) {
                 hashSet.add(s.charAt(pointer++));
                 result = Math.max(result, hashSet.size());
-            } else {
+            } else { 
                 hashSet.remove(s.charAt(index++));
             }
         }
