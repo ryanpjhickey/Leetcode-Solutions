ce8e60cc377fec638e8dd17cc86efdb3c8e03092
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-01 17:36:02 -0800

    add commentary

--- Solutions/Java Solutions/Add Two Numbers/solution.java
+++ Solutions/Java Solutions/Add Two Numbers/solution.java
@@ -11,23 +11,23 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        int carry = 0;
+        int carry = 0; //create our useful vars
         ListNode head = new ListNode(0);
         ListNode pointer = head;
 
-        while (l1 != null || l2 != null || carry != 0) {
-            if (l1 != null) {
+        while (l1 != null || l2 != null || carry != 0) { //check if any of our vars have any value
+            if (l1 != null) { //while l1 has value, add that value to our carry var, and then set l1 to l1.next
                 carry += l1.val;
                 l1 = l1.next;
             }
-            if (l2 != null) {
+            if (l2 != null) { //while l2 has value, add that value to our carry var, and then set l2 to l2.next
                 carry += l2.val;
                 l2 = l2.next;
             }
-            pointer.next = new ListNode(carry % 10);
-            carry /= 10;
-            pointer = pointer.next;
+            pointer.next = new ListNode(carry % 10); //set our pointer.next value to divide our carry var by 10 with remainder
+            carry /= 10; //divde carry var by 10 again
+            pointer = pointer.next; //set our pointer to pointer.next
         }
-        return head.next;
+        return head.next; //return our answer
     }
 };
