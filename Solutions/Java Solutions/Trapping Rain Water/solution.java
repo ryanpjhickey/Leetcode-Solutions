6c6a36a49b9457a3e5e975e6e6831a9b03230a73
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-01 17:45:02 -0800

    update Trapped Water solution to made code more readable, update commentary

--- Solutions/Java Solutions/Trapping Rain Water/solution.java
+++ Solutions/Java Solutions/Trapping Rain Water/solution.java
@@ -1,43 +1,30 @@
 class Solution {
     public int trap(int[] height) {
         
-        int leftMax = 0;
+        int leftMax = 0; //create our useful vars
+        int trap = 0;
+        int rightMax = 0;
+        int leftArr[] = new int[height.length]; //leftArr finds maximum amount on the left of any given location
+        int rightArr[] = new int[height.length]; //rightArr finds maximum amount on the left of any given location
 
-        //leftArr finds maximum amount on the left of any given location
 
-        int leftArr[] = new int[height.length];
-
-        // loop from left to right
-
-        for(int index = 0; index < height.length; index++) {
-            // find maximum of the left
-            if(leftMax < height[index]) {
+        for(int index = 0; index < height.length; index++) { //loop from left to right
+            if(leftMax < height[index]) { //find maximum of the left
                 leftMax = height[index];
             }
             leftArr[index] = leftMax;
         }
-        
-        int rightMax = 0;
-        
-        //rightArr finds maximum amount on the right of any given location
 
-        int rightArr[] = new int[height.length];
-        // loop from right to left
-        for(int index = height.length-1; index >= 0; index--) {
-            
-            // find maximum of the right
-
-            if(rightMax < height[index]) {
+        for(int index = height.length-1; index >= 0; index--) { //loop from right to left
+            if(rightMax < height[index]) { //find maximum of the right
                 rightMax = height[index];
             }
             rightArr[index] = rightMax;
         }
-        
-        int trap = 0;
-        // loops through our previous arrays, caculates, and returns the amount of water trapped
-        for(int index = 0; index  < height.length; index++) {
+
+        for(int index = 0; index  < height.length; index++) { // loops through our previous arrays, caculates, and returns the amount of water trapped
             trap += Math.min(leftArr[index], rightArr[index]) - height[index];
         }
-        return trap;
+        return trap; //return the amount of trapped water
     }
 }
