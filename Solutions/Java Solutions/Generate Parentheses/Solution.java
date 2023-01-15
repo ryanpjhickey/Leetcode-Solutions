989375effa17094ea6830b801380a423495e2719
Ryan <ryanpjhickey@gmail.com>
2023-01-14 21:53:21 -0800

    upload Java solution for Generate Parentheses problem

---
+++ Solutions/Java Solutions/Generate Parentheses/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+
+        List<String> myList = new ArrayList<String>();
+
+        backtrack(myList, "", 0, 0, n);
+        return myList;
+
+    }
+    
+    public void backtrack(List<String> myList, String myString, int openParen, int closeParen, int maximum) {
+        
+        if (myString.length() == maximum*2) {
+            myList.add(myString);
+            return;
+        }
+        
+        if (openParen < maximum) {
+            backtrack(myList, myString+"(", openParen+1, closeParen, maximum);
+        }
+        if( closeParen < openParen) {
+            backtrack(myList, myString+")", openParen, closeParen+1, maximum);
+        }
+    }
+}
