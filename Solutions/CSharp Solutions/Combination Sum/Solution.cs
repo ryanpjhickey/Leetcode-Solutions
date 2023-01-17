e524c9a3d7c383154a4743ac40c62385287d54af
ryanpjhickey <ryanpjhickey@gmail.com>
2023-01-17 15:02:58 -0800

    minor clean up to Solution.cs

--- Solutions/CSharp Solutions/Combination Sum/Solution.cs
+++ Solutions/CSharp Solutions/Combination Sum/Solution.cs
@@ -5,15 +5,13 @@ public class Solution {
           helper(candidates, 0, slate, target);
           return uniqueCombinations;
     }
+
     private void helper(int[] candidates, int index, List<int> slate, int target)
     {
-            
-     
         if(target<0) return;
         if(target==0)
         {
-            uniqueCombinations.Add(new List<int>(slate));
-            
+            uniqueCombinations.Add(new List<int>(slate));  
             return;
         }
         while (index < candidates.Length)
@@ -22,7 +20,6 @@ public class Solution {
           helper(candidates, index, slate, target-candidates[index]);
           index++;
           slate.RemoveAt(slate.Count-1);
-        }
-    
+        } 
     }
 }
