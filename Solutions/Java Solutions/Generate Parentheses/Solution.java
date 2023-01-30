dbce6773b8d6e820920f4c8d1c380b9e20eb755e
Ryan <ryanpjhickey@gmail.com>
2023-01-30 00:27:30 -0800

    update imports

--- Solutions/Java Solutions/Generate Parentheses/Solution.java
+++ Solutions/Java Solutions/Generate Parentheses/Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<String> generateParenthesis(int n) {
 
@@ -7,19 +10,19 @@ class Solution {
         return myList;
 
     }
-    
+
     public void backtrack(List<String> myList, String myString, int openParen, int closeParen, int maximum) {
-        
-        if (myString.length() == maximum*2) {
+
+        if (myString.length() == maximum * 2) {
             myList.add(myString);
             return;
         }
-        
+
         if (openParen < maximum) {
-            backtrack(myList, myString+"(", openParen+1, closeParen, maximum);
+            backtrack(myList, myString + "(", openParen + 1, closeParen, maximum);
         }
-        if( closeParen < openParen) {
-            backtrack(myList, myString+")", openParen, closeParen+1, maximum);
+        if (closeParen < openParen) {
+            backtrack(myList, myString + ")", openParen, closeParen + 1, maximum);
         }
     }
 }
