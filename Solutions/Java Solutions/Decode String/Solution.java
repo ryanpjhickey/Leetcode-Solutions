eb59cfc1e61864e2f70c60d3efc981a5f9fb2945
Ryan <ryanpjhickey@gmail.com>
2023-03-05 23:46:23 -0800

    upload Decode String Java solution

---
+++ Solutions/Java Solutions/Decode String/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public String decodeString(String s) {
+
+        String answer = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        int i = 0;
+        while (i < s.length()) {
+            if (Character.isDigit(s.charAt(i))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(i))) {
+                    count = 10 * count + (s.charAt(i) - '0');
+                    i++;
+                }
+                countStack.push(count);
+            } else if (s.charAt(i) == '[') {
+                resStack.push(answer);
+                answer = "";
+                i++;
+            } else if (s.charAt(i) == ']') {
+                StringBuilder temp = new StringBuilder(resStack.pop());
+                int repeatTimes = countStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    temp.append(answer);
+                }
+                answer = temp.toString();
+                i++;
+            } else {
+                answer += s.charAt(i++);
+            }
+        }
+        return answer;
+    }
+}
